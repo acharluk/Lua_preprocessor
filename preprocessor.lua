bdc06f65149337e18d57545557b651caf89b9a6e
ACharLuk <acharluk@gmail.com>
2016-06-10 14:14:39 +0200

    Opening and closing of files

--- preprocessor.lua
+++ preprocessor.lua
@@ -8,7 +8,21 @@ local tArgs = { ... }
 --File names
 local inFile_name, outFile_name = tArgs[1], tArgs[2]
 
+local inFile, outFile
+
 function main()
+
+    --Open out files
+    inFile = io.open(inFile_name, 'r')
+    outFile = io.open(outFile_name, 'w')
+
+    --Read all data from input file and close it
+    local inFile_data = inFile:read('*a')
+    inFile:close()
+
+
+    --Close output file
+    outFile:close()
 
 end
 
